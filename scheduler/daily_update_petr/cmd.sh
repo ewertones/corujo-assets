c09f846abe22369f5856295c5b85faa93de298b5
Ewerton <ewertonesouza@gmail.com>
2022-07-10 12:41:20 -0300

    Created Cloud Scheduler jobs

---
+++ scheduler/daily_update_petr/cmd.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+name = daily_update_btc
+
+folder = ./scheduler/$name
+
+symbol = PETR3.SAO
+asset = STOCK
+
+gcloud scheduler \
+  jobs create http $name \
+  --location us-central1 \
+  --schedule "0 0 * * *" \
+  --uri "https://us-central1-corujo.cloudfunctions.net/asset_update/?symbol=${symbol}&asset=${asset}"
---
+++ scheduler/daily_update_usd/cmd.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+name = daily_update_btc
+
+folder = ./scheduler/$name
+
+symbol = USD
+asset = FOREX
+
+gcloud scheduler \
+  jobs create http $name \
+  --location us-central1 \
+  --schedule "0 0 * * *" \
+  --uri "https://us-central1-corujo.cloudfunctions.net/asset_update/?symbol=${symbol}&asset=${asset}"
