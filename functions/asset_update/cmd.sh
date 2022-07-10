e3c7bacd1ae1c4d0d16f61d8ecaf34eb4e799ccb
Ewerton <ewertonesouza@gmail.com>
2022-07-10 12:23:10 -0300

    Improved cmd.sh

--- functions/asset_update/cmd.sh
+++ functions/asset_update/cmd.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
+name = asset_update
+
+folder = ./functions/$name
+echo "cmd.sh" > $folder/.gcloudignore
 
 gcloud functions \
-  deploy asset_update \
+  deploy $name \
   --runtime python39 \
   --trigger-http \
   --region us-central1 \
   --set-env-vars 'PROJECT=bi-forecast' \
   --entry-point main \
   --allow-unauthenticated \
-  --source "./functions/asset_update/"
+  --source $folder
