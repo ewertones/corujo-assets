7eb71e93e792bbe131bcef642e316ba7cc5541c9
Ewerton Souza <ewertonesouza@gmail.com>
2022-08-16 21:46:55 -0300

    Changed forecast scheduler timing

--- scheduler/daily_forecast/cmd.sh
+++ scheduler/daily_forecast/cmd.sh
@@ -6,5 +6,5 @@ folder="./scheduler/${name}"
 gcloud scheduler \
   jobs create http $name \
   --location us-central1 \
-  --schedule "0 4 * * *" \
+  --schedule "0 5 * * *" \
   --uri "https://us-central1-corujo.cloudfunctions.net/asset_forecast/"
