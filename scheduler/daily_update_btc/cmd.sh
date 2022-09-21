b318cb7bf717fc476e3a343e69ce845191125330
Ewerton <ewertonesouza@gmail.com>
2022-09-20 23:31:11 -0300

    Fixed wrong parameter

--- scheduler/daily_update_btc/cmd.sh
+++ scheduler/daily_update_btc/cmd.sh
@@ -10,5 +10,5 @@ gcloud scheduler \
   jobs create http $name \
   --location us-central1 \
   --schedule "0 * * * *" \
-  --timeout 540s \
+  --attempt-deadline 540s \
   --uri "https://us-central1-corujo.cloudfunctions.net/asset_update/?symbol=${symbol}&asset=${asset}"
